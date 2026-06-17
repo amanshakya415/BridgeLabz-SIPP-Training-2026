563cfbacae8d865f100e0df4444ded8bd9daca78
Aman <amanshakya415@gmail.com>
2026-06-17 21:36:51 +0530

    print N to 1 numbers

---
+++ core-java-practice/gcr-codebase/Recursion/Recursion-Practise/PrintNumbersFromNTo1.java
@@ -0,0 +1,17 @@
+public class PrintNumbersFromNTo1 {
+    public static void main(String[] args) {
+        int N = 5; 
+        printNumbers(N);
+    }
+
+    public static void printNumbers(int N) {
+        // Base case: if N is less than 1, return
+        if (N < 1) {
+            return;
+        }
+        // Print the current number
+        System.out.print(N + " ");
+        // Recursive call with N decremented by 1
+        printNumbers(N - 1);
+    }
+}
